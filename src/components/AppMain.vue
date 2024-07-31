91fe0a23bebb7660b62700803273381d92e0db70
lucadisanto7 <lucadisanto47@gmail.com>
2024-07-31 16:38:48 +0200

    [UPD] Added cards

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,37 @@
-<script>
-export default {
-    
-}
-</script>
 <template>
     <div>
-        
+      <main class="background-orange">
+        <div class="card-container" id="card-container">
+          
+        </div>
+      </main>
     </div>
-</template>
-<style lang="scss">
-    
-</style>
+  </template>
+  
+  <script>
+  
+  </script>
+  
+  <style lang="scss">
+  .background-orange {
+    background-color: #d48e39;
+  }
+  .card-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+  .card {
+    margin: 10px;
+    padding: 10px;
+    border: 1px solid #ccc;
+    background-color: #fff;
+    width: 200px;
+  }
+  .card img {
+    width: 100%;
+  }
+  .card h3 {
+    font-size: 1.2em;
+  }
+  </style>
