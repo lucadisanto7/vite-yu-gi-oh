ccaa270e4663b35902e173e14281285afcca0f66
lucadisanto7 <lucadisanto47@gmail.com>
2024-07-31 16:22:01 +0200

    [UPD] Added header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,9 +5,25 @@ export default {
 </script>
 <template>
     <div>
-        
+        <div class="containers">
+            <div class="row">
+                <div class="col-12">
+                    <div class="content my-2 ms-5">
+                        <img src="https://1000logos.net/wp-content/uploads/2024/02/Yu-Gi-Oh-Logo-2001.png" alt="logo">
+                        <h1>Yu-Gi-Oh Api</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
-<style>
-    
+<style lang="scss">
+  img{
+    width: 100px;
+  }
+  h1{
+    display: inline-block;
+    vertical-align: middle;
+    margin-left: 25px;
+  }  
 </style>
